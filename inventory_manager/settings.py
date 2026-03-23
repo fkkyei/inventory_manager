1c47627ae3d91b595b65678c59ec9a0bbb125572
fkkyei <kwasikyeigyamfi@outlook.com>
2026-03-23 08:50:47 +0000

    supa

--- inventory_manager/settings.py
+++ inventory_manager/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,11 +91,11 @@ WSGI_APPLICATION = 'inventory_manager.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres.bldvcwwjuhawdpwnagba',
-        'PASSWORD': 'ECGNSS123@@',
-        'HOST': 'aws-1-eu-west-1.pooler.supabase.com',
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
